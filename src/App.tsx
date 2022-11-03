16c891301325d4eef452beb5477e3ae44f37eb21
Saiket Das <saiketdas02@gmail.com>
2022-11-03 16:56:46 +0600

    Axios, redux, bootstrap, router package install

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,46 @@
-import React, {useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import axios from "axios";
-
+import Button from "react-bootstrap/Button";
 
 const App = () => {
-    useEffect(() => {
-        var data = JSON.stringify({
-            "username": "12345",
-            "password": "12345"
-        });
+  useEffect(() => {
+    var data = JSON.stringify({
+      username: "12345",
+      password: "12345",
+    });
 
-        var config = {
-            method: 'post',
-            url: 'https://ajkermenu.com:8443/api/credentials/login',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Basic Y2hlZjpjaGVm'
-            },
-            data : data
-        };
+    var config = {
+      method: "post",
+      url: "https://ajkermenu.com:8443/api/credentials/login",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic Y2hlZjpjaGVm",
+      },
+      data: data,
+    };
 
-        axios(config)
-            .then(function (response) {
-                console.log(JSON.stringify(response.data));
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-
-    }, []);
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <>
+      <Button variant="outline-primary">Primary</Button>{" "}
+      <Button variant="outline-secondary">Secondary</Button>{" "}
+      <Button variant="outline-success">Success</Button>{" "}
+      <Button variant="outline-warning">Warning</Button>{" "}
+      <Button variant="outline-danger">Danger</Button>{" "}
+      <Button variant="outline-info">Info</Button>{" "}
+      <Button variant="outline-light">Light</Button>{" "}
+      <Button variant="outline-dark">Dark</Button>
+    </>
   );
 };
 
