a1cb88df56350969795bfe1bcc56db7944947484
Saiket Das <saiketdas02@gmail.com>
2022-11-08 17:22:07 +0600

    App.js is wrapped in <Provider>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import Login from "./pages/auth/login/Login";
 import Home from "./pages/home/Home";
+import { store } from "./store/store";
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="home" element={<Home />}></Route>
-        <Route path="login" element={<Login />}></Route>
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="home" element={<Home />}></Route>
+          <Route path="login" element={<Login />}></Route>
+        </Routes>
+      </Provider>
     </>
   );
 };
